8acd3689191cdd7e5090cb9a89d252693355af64
Gustavo Larsen <guslarsen@gmail.com>
2023-06-10 16:05:12 -0300

    feat: dynamic list jobs getfrfom JSON data file

--- src/app/components/JobCard/JobCard.component.tsx
+++ src/app/components/JobCard/JobCard.component.tsx
@@ -1,23 +1,64 @@
 import Image from 'next/image';
 import styles from './Jobcard.module.scss';
 
-const JobCard = () => {
+interface IJobProps {
+  job: {
+    id: number;
+    company: string;
+    logo: string;
+    new: boolean;
+    featured: boolean;
+    position: string;
+    role: string;
+    level: string;
+    postedAt: string;
+    contract: string;
+    location: string;
+    languages: Array<string>;
+    tools: Array<string>;
+  };
+}
+
+const JobCard = ({ job }: IJobProps) => {
   return (
-    <li className={styles.jobCard}>
+    <li
+      className={`${
+        job.featured
+          ? `${styles.jobCard} ${styles.featured}`
+          : `${styles.jobCard}`
+      }`}
+    >
       <Image
         className={styles.logo}
-        src="./images/insure.svg"
+        src={job.logo}
         width={48}
         height={48}
         alt="Logo da empresa"
       />
-      <h1>Empresa</h1>
-      <h2>Nome da vaga</h2>
-      <p>
-        <span>1d ago</span>
-        <span>Part time</span>
-        <span>Remote</span>
-      </p>
+      <div className={styles.content}>
+        <div className={styles.headerJob}>
+          <h1 className={styles.companyName}>{job.company}</h1>
+          {job.new && <span className={styles.new}>NEW!</span>}
+          {job.featured && <span className={styles.featured}>FEATURED</span>}
+        </div>
+
+        <h2 className={styles.jobPosition}>{job.position}</h2>
+        <p className={styles.info}>
+          <span>{job.postedAt}</span>
+          <span>{job.contract}</span>
+          <span>{job.location}</span>
+        </p>
+      </div>
+
+      <section className={styles.languages}>
+        {job.languages.map((language, index) => {
+          return (
+            <span className={styles.language} key={index}>
+              {language}
+            </span>
+          );
+        })}
+      </section>
     </li>
   );
 };
--- src/app/components/JobList/JobList.component.tsx
+++ src/app/components/JobList/JobList.component.tsx
@@ -1,12 +1,14 @@
 import styles from './JobList.module.scss';
 import JobCard from '../JobCard/JobCard.component';
 
+import { Jobs } from '@/app/services/Jobs.service';
+
 const JobList = () => {
   return (
     <ul className={styles.jobList}>
-      <JobCard />
-      <JobCard />
-      <JobCard />
+      {Jobs.map((job) => {
+        return <JobCard key={job.id} job={job} />;
+      })}
     </ul>
   );
 };
